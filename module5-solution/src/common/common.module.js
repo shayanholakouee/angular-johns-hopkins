0328dc70670378739d543ede894ba8e964cb1866
shayan holakouee <shayanholakuee@gmail.com>
2022-09-04 01:54:46 +0400

    modify

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://kellis9-module5.herokuapp.com/')
+.constant('ApiPath', 'https://shayanhlk.herokuapp.com/')
 .config(config);
 
 config.$inject = ['$httpProvider'];
